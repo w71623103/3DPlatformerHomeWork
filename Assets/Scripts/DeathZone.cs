408e11ca2b07ae14ab79eb0f38ebb6d4729aac02
Yinqi Wang <w71623103@163.com>
2022-04-04 20:36:44 -0400

    +Map refined +Enemy Destroy

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DeathZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Player":
+                Debug.Log("ToStartScene");
+                break;
+            case "Enemy":
+                Destroy(collision.gameObject);
+                break;
+        }
+    }
+}
